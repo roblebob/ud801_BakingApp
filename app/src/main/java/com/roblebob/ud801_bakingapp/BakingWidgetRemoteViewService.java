0cf19c0ffecb0cf233e6e0b367aa4924d0b15bfd
roblebob <robert.koppisch@gmail.com>
2021-10-29 22:21:24 +0200

    overall cleanup 2

--- app/src/main/java/com/roblebob/ud801_bakingapp/BakingWidgetRemoteViewService.java
+++ app/src/main/java/com/roblebob/ud801_bakingapp/BakingWidgetRemoteViewService.java
@@ -30,66 +30,43 @@ public class BakingWidgetRemoteViewService extends RemoteViewsService {
         return new BakingWidgetItemFactory(getApplicationContext(), intent);
     }
 
-
     @WorkerThread
     static class BakingWidgetItemFactory implements RemoteViewsFactory{
 
         private final Context mContext;
         private final int mAppWidgetId;
         private List<String> mList = new ArrayList<>();
-        //private final RecipeDao mRecipeDao;
-
 
         BakingWidgetItemFactory(Context context, Intent intent) {
             mContext = context;
             mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-//            AppDatabase appDatabase = AppDatabase.getInstance(mContext);
-//            mRecipeDao = appDatabase.recipeDao();
         }
 
-        @Override
-        public void onCreate() {
-            // connect to data source
-            //mList = mRecipeDao.loadRecipeNameList();
-
-//            Executors.getInstance().diskIO().execute( () -> {
-//                AppDatabase appDatabase = AppDatabase.getInstance(mContext);
-//                RecipeDao recipeDao = appDatabase.recipeDao();
-//                mList = recipeDao.loadRecipeNameList();
-//            });
+        @Override public void onCreate() {}
+        @Override public void onDestroy() {}
+        @Override public int getCount() {
+            return mList.size();
+        }
+        @Override public RemoteViews getLoadingView() {
+            return null;
+        }
+        @Override public int getViewTypeCount() {
+            return 1;
+        }
+        @Override public long getItemId(int i) {
+            return i;
+        }
+        @Override public boolean hasStableIds() {
+            return true;
         }
 
         @Override
         public void onDataSetChanged() {
-            //refresh data
-            //Date date = new Date();
-            //String timeFormatted = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(date);
-
-            //mList = new ArrayList<>(Arrays.asList(exampleData));
-
-            //mList =  mRecipeDao.loadRecipeNameList();
-
-            //mList = mList.stream().map((s) -> s + "\n" + timeFormatted).collect(Collectors.toList());
-
-
             Executors.getInstance().diskIO().execute( () -> {
                 AppDatabase appDatabase = AppDatabase.getInstance(mContext);
                 RecipeDao recipeDao = appDatabase.recipeDao();
                 mList = recipeDao.loadRecipeNameList();
             });
-
-
-            //SystemClock.sleep(3000); // to simulate
-        }
-
-        @Override
-        public void onDestroy() {
-            // close connection to data source
-        }
-
-        @Override
-        public int getCount() {
-            return mList.size();
         }
 
         @Override
@@ -97,36 +74,14 @@ public class BakingWidgetRemoteViewService extends RemoteViewsService {
             RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.baking_widget_single_item_recipe_name);
             views.setTextViewText(R.id.baking_widget_single_item_recipe_name_tv, mList.get(i));
 
-
             Intent fillIntent = new Intent();
             fillIntent.putExtra(EXTRA_ITEM_RECIPE_NAME, mList.get(i));
             fillIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
             views.setOnClickFillInIntent(R.id.baking_widget_single_item_recipe_name_tv, fillIntent);
 
-            // heavy loading possible
-            //SystemClock.sleep(500);
             return views;
         }
 
-        @Override
-        public RemoteViews getLoadingView() {
-            return null;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return 1;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
     }
 
 }
