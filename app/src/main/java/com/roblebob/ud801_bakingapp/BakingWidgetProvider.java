09b07daddacb88dd9c94daa8b47378c7e08b56f2
roblebob <robert.koppisch@gmail.com>
2021-10-29 22:15:11 +0200

    overall cleanup

--- app/src/main/java/com/roblebob/ud801_bakingapp/BakingWidgetProvider.java
+++ app/src/main/java/com/roblebob/ud801_bakingapp/BakingWidgetProvider.java
@@ -1,7 +1,5 @@
 package com.roblebob.ud801_bakingapp;
 
-//import static com.roblebob.ud801_bakingapp.BakingWidgetConfig.KEY_BUTTON_TEXT;
-//import static com.roblebob.ud801_bakingapp.BakingWidgetConfig.SHARED_PRES;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -29,10 +27,6 @@ public class BakingWidgetProvider extends AppWidgetProvider {
 
     public static final String ACTION_REFRESH = "actionRefresh";
     public static final String EXTRA_ITEM_RECIPE_NAME = "extraItemRecipeName";
-    public static final String EXTRA_CURRENT_RECIPE_NAME = "extraCurrentRecipeName";
-
-
-    //private String mCurrentRecipeName;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -48,33 +42,25 @@ public class BakingWidgetProvider extends AppWidgetProvider {
             PendingIntent cakePendingIntent = PendingIntent.getActivity(context, 0, cakeIntent, 0);
             views.setOnClickPendingIntent( R.id.widget_image_view, cakePendingIntent);
 
-
-/*
-            SharedPreferences prefs = context.getSharedPreferences(SHARED_PRES, Context.MODE_PRIVATE);
-            String buttonText = prefs.getString(KEY_BUTTON_TEXT + appWidgetId, "press me");
-            views.setCharSequence(R.id.widget_ingredient_button, "setText", buttonText);
-*/
-
+            // refresh action to reset new current recipe name
             Intent clickIntent = new Intent(context, BakingWidgetProvider.class);
             clickIntent.setAction(ACTION_REFRESH);
             PendingIntent clickPendingIntent = PendingIntent.getBroadcast(context, 0, clickIntent, 0);
             views.setPendingIntentTemplate(R.id.widget_recipe_name_list_view, clickPendingIntent);
 
-
+            // left (recipe name list)
             Intent serviceIntent = new Intent(context, BakingWidgetRemoteViewService.class);
             serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
             serviceIntent.setData(Uri.parse( serviceIntent.toUri( Intent.URI_INTENT_SCHEME)));
             views.setRemoteAdapter(R.id.widget_recipe_name_list_view, serviceIntent);
             views.setEmptyView(R.id.widget_recipe_name_list_view, R.id.widget_recipe_name_empty_tv);
 
-
+            // right (ingredient list)
             Intent serviceIntentIngredients = new Intent(context, BakingWidgetRemoteViewServiceIngredients.class);
             serviceIntentIngredients.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            //serviceIntentIngredients.putExtra(EXTRA_CURRENT_RECIPE_NAME, mCurrentRecipeName);
             serviceIntentIngredients.setData(Uri.parse(serviceIntentIngredients.toUri(Intent.URI_INTENT_SCHEME)));
             views.setRemoteAdapter(R.id.widget_ingredient_list_view, serviceIntentIngredients);
             views.setEmptyView(R.id.widget_ingredient_list_view, R.id.widget_ingredient_empty_tv);
-
 
 
             Bundle appWidgetsOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
@@ -110,12 +96,26 @@ public class BakingWidgetProvider extends AppWidgetProvider {
     }
 
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
 
+        if (ACTION_REFRESH.equals(intent.getAction())) {
 
+            String currentRecipeName = intent.getStringExtra(EXTRA_ITEM_RECIPE_NAME);
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
 
+            Executors.getInstance().diskIO().execute( () -> {
+                AppDatabase appDatabase = AppDatabase.getInstance(context);
+                AppStateDao appStateDao = appDatabase.appStateDao();
+                appStateDao.insert(new AppState("current_recipe_name", currentRecipeName));
+            });
 
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_ingredient_list_view);
+        }
 
-
+        super.onReceive(context, intent);
+    }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
@@ -127,49 +127,4 @@ public class BakingWidgetProvider extends AppWidgetProvider {
         });
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        Log.e(this.getClass().getSimpleName(), "onEnable()");
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        Log.e(this.getClass().getSimpleName(), "onDisabled()");
-    }
-
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-
-        if (ACTION_REFRESH.equals(intent.getAction())) {
-
-            String currentRecipeName = intent.getStringExtra(EXTRA_ITEM_RECIPE_NAME);
-
-
-            Log.e(this.getClass().getSimpleName() + "  onReceive(): ", "[[" + currentRecipeName + "]]");
-
-
-            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget_provider);
-
-
-
-            Executors.getInstance().diskIO().execute( () -> {
-                AppDatabase appDatabase = AppDatabase.getInstance(context);
-                AppStateDao appStateDao = appDatabase.appStateDao();
-                appStateDao.insert(new AppState("current_recipe_name", currentRecipeName));
-            });
-
-            //SystemClock.sleep(1000);
-
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            // appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_recipe_name_list_view);
-            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_ingredient_list_view);
-        }
-
-
-
-        super.onReceive(context, intent);
-    }
 }
--- app/src/main/java/com/roblebob/ud801_bakingapp/BakingWidgetRemoteViewServiceIngredients.java
+++ app/src/main/java/com/roblebob/ud801_bakingapp/BakingWidgetRemoteViewServiceIngredients.java
@@ -1,11 +1,8 @@
 package com.roblebob.ud801_bakingapp;
 
-import static com.roblebob.ud801_bakingapp.BakingWidgetProvider.EXTRA_CURRENT_RECIPE_NAME;
-
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
@@ -30,45 +27,24 @@ public class BakingWidgetRemoteViewServiceIngredients extends RemoteViewsService
     @WorkerThread
     static class BakingWidgetItemFactoryIngredients implements RemoteViewsFactory {
         private final Context mContext;
-        private int mAppWidgetId;
+        private final int mAppWidgetId;
         private List<Ingredient> mIngredientList = new ArrayList<>();
         private int mSize;
-//        private final IngredientDao mIngredientDao;
-//        private final AppStateDao mAppStateDao;
-        //private String mCurrentRecipeName;
+
 
         BakingWidgetItemFactoryIngredients(Context context, Intent intent) {
             mContext = context;
             mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-
-//            AppDatabase appDatabase = AppDatabase.getInstance(mContext);
-//            mIngredientDao = appDatabase.ingredientDao();
-//            mAppStateDao = appDatabase.appStateDao();
-//            mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-//
-            //mCurrentRecipeName = intent.getStringExtra(EXTRA_CURRENT_RECIPE_NAME);
-            //Log.e(this.getClass().getSimpleName(), "constructor: [[" + mCurrentRecipeName + "]]");
-
         }
 
+        @Override public void onCreate() {}
+        @Override public int getCount() { return mSize; }
+        @Override public RemoteViews getLoadingView() { return null; }
+        @Override public int getViewTypeCount() { return 1; }
+        @Override public long getItemId(int i) { return i; }
+        @Override public boolean hasStableIds() { return true; }
+        @Override public void onDestroy() { }
 
-        @Override
-        public void onCreate() {
-
-//            Executors.getInstance().diskIO().execute( () -> {
-//                        AppDatabase appDatabase = AppDatabase.getInstance(mContext);
-//                        AppStateDao appStateDao = appDatabase.appStateDao();
-//                        IngredientDao ingredientDao = appDatabase.ingredientDao();
-//                        mCurrentRecipeName = appStateDao.loadCurrentRecipeName();
-//                        SystemClock.sleep(500);
-//                        mIngredientList = ingredientDao.loadIngredients(mCurrentRecipeName);
-//                    });
-
-//            mCurrentRecipeName = mAppStateDao.loadCurrentRecipeName();
-//            mIngredientList = mIngredientDao.loadIngredients(mCurrentRecipeName);
-
-            //Log.e(this.getClass().getSimpleName(), "onCreate():  [[" + mCurrentRecipeName + "]]");
-        }
 
         @Override
         public void onDataSetChanged() {
@@ -83,21 +59,13 @@ public class BakingWidgetRemoteViewServiceIngredients extends RemoteViewsService
                 Log.e(this.getClass().getSimpleName(), "onDataSetChanged(): [[" + currentRecipeName + "]]");
                 Log.e(this.getClass().getSimpleName(), "size: " + mSize);
             });
-
-//            mCurrentRecipeName = mAppStateDao.loadCurrentRecipeName();
-//            mIngredientList = mIngredientDao.loadIngredients(mCurrentRecipeName);
-
-            //Log.e(this.getClass().getSimpleName(), "onDataSetChanged(): [[" + mCurrentRecipeName + "]]");
-            //Log.e(this.getClass().getSimpleName(), "size: " + mIngredientList.size());
         }
 
         @Override
         public RemoteViews getViewAt(int i) {
             if (i >= mSize) return null;
+
             Ingredient ingredient = mIngredientList.get(i);
-
-            RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.baking_widget_single_item_ingredient);
-
             String s = ingredient.getName() +
                     "  " +
                     ((ingredient.getQuantity() - ((int) ingredient.getQuantity()) == 0.0) ?
@@ -107,18 +75,9 @@ public class BakingWidgetRemoteViewServiceIngredients extends RemoteViewsService
                     " " +
                     ingredient.getMeasure();
 
+            RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.baking_widget_single_item_ingredient);
             views.setTextViewText(R.id.baking_widget_single_item_ingredient_tv, s);
-
             return views;
         }
-
-        @Override public int getCount() { return mSize; }
-        @Override public RemoteViews getLoadingView() { return null; }
-        @Override public int getViewTypeCount() { return 1; }
-        @Override public long getItemId(int i) { return i; }
-        @Override public boolean hasStableIds() { return true; }
-        @Override public void onDestroy() { }
-
-
     }
 }
