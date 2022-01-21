1ed1d2080afeac832b10649f00af02db537f0110
roblebob <robert.koppisch@gmail.com>
2022-01-21 12:46:53 +0100

    first step

--- app/src/main/java/com/roblebob/ud801_bakingapp/ui/MainActivity.java
+++ app/src/main/java/com/roblebob/ud801_bakingapp/ui/MainActivity.java
@@ -1,21 +1,46 @@
 package com.roblebob.ud801_bakingapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.PictureInPictureParams;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Rational;
+import android.view.View;
 
 import com.roblebob.ud801_bakingapp.R;
-import com.roblebob.ud801_bakingapp.model.Recipe;
-import com.roblebob.ud801_bakingapp.model.Step;
 
 public class MainActivity extends AppCompatActivity
 {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onUserLeaveHint() {
+
+        View rootOfStepFragment = (View) findViewById(R.id.fragment_step_root);
+
+        if ( rootOfStepFragment != null) {
+
+            Log.e(TAG, "entered pip");
+
+            enterPictureInPictureMode(
+                    new PictureInPictureParams
+                            .Builder()
+                            .setAspectRatio(new Rational(16, 9))
+                            .build()
+            );
+        } else {
+            Log.e(TAG, "avoided entering pip");
+            super.onUserLeaveHint();
+        }
+
+    }
+
 }
